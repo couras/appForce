860481499a26f74fc180cf9021a75bfab3e1b4de
Kouras <deniscouras@yahoo.com.br>
2016-11-19 02:39:56 -0200

    dfgdghd

---
+++ appForce/Framework/ManagerCRUD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appForce.Framework
+{
+    class ManagerCRUD
+    {
+        public string createInsert()
+        {
+            return "insert into";
+        }
+    }
+}
